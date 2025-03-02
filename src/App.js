b76f2ae61e55b8ae2e325047fdb1bbe443fb2cba
ESHA <eshaiyer1511@gmail.com>
2025-03-02 12:01:38 +0530

    Added scores

--- src/App.js
+++ src/App.js
@@ -20,15 +20,15 @@ function App() {
   const [players, setPlayers] = useState([
     { id: 1, name: "Basic Sciences", score: 0 },
     { id: 2, name: "Chemical", score: 0 },
-    { id: 3, name: "Civil", score: 0 },
-    { id: 4, name: "CSE", score: 0 },
-    { id: 5, name: "ECE", score: 0 },
-    { id: 6, name: "EEE", score: 0 },
-    { id: 7, name: "IT/AI", score: 0 },
+    { id: 3, name: "Civil", score: 300 },
+    { id: 4, name: "CSE", score: 400 },
+    { id: 5, name: "ECE", score: 400 },
+    { id: 6, name: "EEE", score: 200 },
+    { id: 7, name: "IT/AI", score: 200 },
     { id: 8, name: "MACS/CDS", score: 0 },
-    { id: 9, name: "Mechanical", score: 0 },
+    { id: 9, name: "Mechanical", score: 300 },
     { id: 10, name: "Mining", score: 0 },
-    { id: 11, name: "MME", score: 0 },
+    { id: 11, name: "MME", score: 100 },
     { id: 12, name: "SOM", score: 0 },
   ]);
 
